de4f84b5690fb2baf0ff318b20707a852918b137
Your Name <patryck.technology@gmail.com>
2025-02-12 22:51:53 -0300

    add dia 8

---
+++ day8/check_idade.py
@@ -0,0 +1,6 @@
+#Solicitar a idade do usuario e verificar se ele pode dirigir
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Voce pode dirigir")
+else:
+    print("Voce não pode dirigir")
